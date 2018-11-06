672d1c3830a05f1e269d4770fb3b086c1a09ff23
Rajan <rajansaini691@gmail.com>
2018-11-06 00:17:26 -0800

    Added more effects

--- sfx.py
+++ sfx.py
@@ -4,16 +4,30 @@ import pysox
 infile = pysox.CSoxStream('G.wav')
 
 # Creates a new output file to be written to
-outfile = pysox.CSoxStream('out.wav','w',infile.get_signal())
+outfile = pysox.CSoxStream('G_Modified.wav','w',infile.get_signal())
 
 # Creates a new chain of effects
 chain = pysox.CEffectsChain(infile, outfile)
 
-# Creates a new effect
-effect = pysox.CEffect("chorus", [b'0.7', b'0.9', b'55', b'0.4', b'0.25', b'2', b'-t'])
+# Adds chorus 
+chorus = pysox.CEffect("chorus", [b'0.7', b'0.9', b'55', b'0.4', b'0.25', b'2', b'-t'])
+chain.add_effect(chorus)
 
-# Adds the created effect to the chain
-chain.add_effect(effect)
+# Adds reverb
+reverb = pysox.CEffect("reverb", [b'50'])
+chain.add_effect(reverb)
+
+# Adds echo
+echo = pysox.CEffect("echo", [b'0.8', b'0.88', b'1000', b'0.4'])
+chain.add_effect(echo)
+
+# Adds pitch bending (tremolo)
+tremolo = pysox.CEffect("tremolo", [b'5', b'50'])
+chain.add_effect(tremolo)
+
+# Reverses the sound 
+# reverse = pysox.CEffect("reverse", [])
+# chain.add_effect(reverse)
 
 # Writes all effects on the chain to the output file
 chain.flow_effects()
